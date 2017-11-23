d467908170c71d993e846279542ef6dc225783a5
YiHyunJin <hj0412.yi@samsung.com>
2017-11-22 20:18:47 -0800

    apps/example/kernel_sample: Put a workqueue in kernel_sample When run kernel_sample, also workqueue to test.

--- apps/examples/kernel_sample/workqueue.c
+++ apps/examples/kernel_sample/workqueue.c
@@ -20,9 +20,12 @@
  ****************************************************************************/
 
 #include <tinyara/config.h>
-#include <stdio.h>
 #include <tinyara/clock.h>
 #include <tinyara/wqueue.h>
+#include <sys/types.h>
+#include <stdio.h>
+
+#if defined(CONFIG_SCHED_WORKQUEUE) || defined(CONFIG_LIB_USRWORK)
 
 /****************************************************************************
  * Definitions
@@ -32,6 +35,8 @@
  * Private Data
  ****************************************************************************/
 
+uint64_t wq_order[4];
+
 /****************************************************************************
  * Private Functions
  ****************************************************************************/
@@ -40,7 +45,9 @@ static void wq_test1(FAR void *arg)
 	systime_t cur_time = 0;
 	cur_time = clock_systimer();
 
-	printf("test1 is excuted at (%d) ticks\n", cur_time);
+	wq_order[1] = (uint64_t)cur_time;
+
+	printf("workqueue_test: test1 is excuted at (%lld) ticks\n", (uint64_t)cur_time);
 }
 
 static void wq_test2(FAR void *arg)
@@ -48,23 +55,29 @@ static void wq_test2(FAR void *arg)
 	systime_t cur_time = 0;
 	cur_time = clock_systimer();
 
-	printf("test2 is excuted at (%d) ticks\n", cur_time);
+	wq_order[0] = (uint64_t)cur_time;
+
+	printf("workqueue_test: test2 is excuted at (%lld) ticks\n", (uint64_t)cur_time);
 }
 
 static void wq_test3(FAR void *arg)
 {
 	systime_t cur_time = 0;
 	cur_time = clock_systimer();
+	
+	wq_order[2] = (uint64_t)cur_time;
 
-	printf("test3 is excuted at (%d) ticks\n", cur_time);
+	printf("workqueue_test: test3 is excuted at (%lld) ticks\n", (uint64_t)cur_time);
 }
 
 static void wq_test4(FAR void *arg)
 {
 	systime_t cur_time = 0;
 	cur_time = clock_systimer();
+	
+	wq_order[3] = (uint64_t)cur_time;
 
-	printf("test4 is excuted at (%d) ticks\n", cur_time);
+	printf("workqueue_test: test4 is excuted at (%lld) ticks\n", (uint64_t)cur_time);
 }
 
 /****************************************************************************
@@ -72,15 +85,12 @@ static void wq_test4(FAR void *arg)
  ****************************************************************************/
 
 /****************************************************************************
- * workqueue_main
+ * workqueue_test
  ****************************************************************************/
 
-#ifdef CONFIG_BUILD_KERNEL
-int main(int argc, FAR char *argv[])
-#else
-int workqueue_main(int argc, char *argv[])
-#endif
+int workqueue_test(void)
 {
+	int order;
 	systime_t cur_time;
 	struct work_s *test_wq1;
 	struct work_s *test_wq2;
@@ -94,17 +104,34 @@ int workqueue_main(int argc, char *argv[])
 
 	cur_time = clock_systimer();
 
-	printf("current time is (%d)\n", cur_time);
-	printf("test1 is queued and will excute it after 50 ticks\n");
+	printf("workqueue_test: current time is (%lld)\n", (uint64_t)cur_time);
+	printf("workqueue_test: test1 is queued and will excute it after 50 ticks\n");
 	work_queue(HPWORK, test_wq1, wq_test1, NULL, 50);
 
-	printf("test2 is queued and will excute it after 30 ticks\n");
+	printf("workqueue_test: test2 is queued and will excute it after 30 ticks\n");
 	work_queue(HPWORK, test_wq2, wq_test2, NULL, 30);
 
-	printf("test3 is queued and will excute it after 70 ticks\n");
+	printf("workqueue_test: test3 is queued and will excute it after 70 ticks\n");
 	work_queue(HPWORK, test_wq3, wq_test3, NULL, 70);
 
-	printf("test4 is queued and will excute it after 90 ticks\n");
+	printf("workqueue_test: test4 is queued and will excute it after 90 ticks\n");
 	work_queue(HPWORK, test_wq4, wq_test4, NULL, 90);
+	
+	sleep(1);
+
+	free(test_wq1);
+	free(test_wq2);
+	free(test_wq3);
+	free(test_wq4);
+
+	for (order = 0; order < 3; order++) {
+		if (wq_order[order] > wq_order[order + 1]) {
+			printf("workqueue_test: fail\n");
+			return ERROR;
+		}
+	}	
+	printf("workqueue_test: success\n");
 	return 0;
 }
+
+#endif
